52918c147d390fc540a322b43604435eecc16bca
Nate Miller <natekmiller96@gmail.com>
2018-11-09 18:33:06 +0000

    automatically backed up by learn

--- src/Timer.js
+++ src/Timer.js
@@ -7,8 +7,10 @@ class Timer extends Component {
     color: '#'+Math.floor(Math.random()*16777215).toString(16)
   }
 
-  // add your code here
 
+componentDidMount(){
+  this.interval = setInterval(...)
+}
 
 
 
